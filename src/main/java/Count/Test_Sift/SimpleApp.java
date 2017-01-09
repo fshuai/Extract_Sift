a8c5a51865acd533c6a20bf955d931278f329166
root <root@localhost.localdomain>
2017-01-09 22:20:46 +0800

    RDD<seriImage>

--- src/main/java/Count/Test_Sift/SimpleApp.java
+++ src/main/java/Count/Test_Sift/SimpleApp.java
@@ -1,37 +1,37 @@
 package Count.Test_Sift;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.ImageIcon;
-
+import javax.imageio.ImageIO;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class SimpleApp {
 	
-	public static List<ImageIcon> getImages(String path){
+	public static List<SeriImage> getImages(String path) throws IOException{
 		File file=new File(path);
 		File[] tempList=file.listFiles();
-		List<ImageIcon> imageList=new ArrayList<ImageIcon>();
+		List<SeriImage> imageList=new ArrayList<SeriImage>();
 		for(int i=0;i<tempList.length;i++){
 			if(tempList[i].getName().endsWith(".jpg") || tempList[i].getName().endsWith(".png")){
-				ImageIcon tmp=new ImageIcon(tempList[i].getName());
+				SeriImage tmp=new SeriImage(ImageIO.read(tempList[i]));
 				imageList.add(tmp);
 			}
 		}
 		return imageList;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		SparkConf conf=new SparkConf().setAppName("SimpleApp");
 		JavaSparkContext sc=new JavaSparkContext(conf);
 		String filepath="/root/image/";
-		List<ImageIcon> images=null;
-		JavaRDD <ImageIcon> result=null;
+		List<SeriImage> images=null;
+		JavaRDD <SeriImage> result=null;
 		images=getImages(filepath);
 		result=sc.parallelize(images);
 		result.cache();
